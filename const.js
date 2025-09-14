6067b7073151319d4468832e12d48862ae2e8285
shobeedev <choubimohammed99@gmail.com>
2025-09-14 19:00:41 +0100

    test 3

---
+++ const.js
@@ -0,0 +1,5 @@
+const n1 = 'shobee4ever'
+const n2 = 'test4test0'
+const n3 = 'nakamora'
+
+module.exports = {n1,n2,n3}
---
+++ func.js
@@ -0,0 +1,4 @@
+const sayWe = (name) =>{
+    console.log(`Welecome to our website ${name}`)
+}
+module.exports = sayWe

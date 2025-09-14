9cee9d7aea1533b58602895274242afc388eaa38
shobeedev <choubimohammed99@gmail.com>
2025-09-14 12:07:18 +0100

    test 2

---
+++ 2-globale-js.js
@@ -0,0 +1,17 @@
+//Globale
+
+//__dirname -> path to current dir
+//__filename -> file name
+// require -> function to use modules (CommonJs)
+// module -> info about current module (file)
+// process -> info about environmment where the programe is being executed
+
+//console.log(__dirname)
+//console.log(__filename)
+//console.log(require)
+//console.log(module)
+//console.log(process)
+
+setInterval(()=>{
+    console.log("hello wold")
+},1000) //this timeout use milliseconds
